22a973a67ee2a3d2ea1893ecbd2c3e61f1284b9f
devansh141pal <devanshpal75@gmail.com>
2025-02-26 18:22:26 +0530

    Basic Server Created

---
+++ Backend/server.js
@@ -0,0 +1,19 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
